5d3f2015d0b37483d7602fd8290a41cca00619d9
Chaim B. Sommers <cb@compute.co.il>
2020-07-22 23:54:47 +0300

    Initial add.

---
+++ ConnectAccess/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ConnectAccess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            using (var con = new OleDbConnection(this.textBox1.Text))
+            {
+                using(var adptr = new OleDbDataAdapter(this.textBox2.Text, con))
+                {
+                    DataSet ds = new DataSet();
+                    adptr.Fill(ds);
+                    this.dataGridView1.DataSource = ds.Tables[0];
+                    this.dataGridView1.Refresh();
+                }
+            }
+        }
+    }
+}
